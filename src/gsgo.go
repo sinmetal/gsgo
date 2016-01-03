c454e86db038f51dd81924a507eb82a400745c9b
Shingo Ishimura <metal.tie@gmail.com>
2016-01-03 22:32:45 +0900

    GCSからファイルを読み込みDSに保存する叩き台を作成した

--- src/gsgo.go
+++ src/gsgo.go
@@ -2,6 +2,8 @@ package gsgo
 
 import (
 	"bufio"
+	"encoding/csv"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"strconv"
@@ -45,47 +47,78 @@ func handlerFileRead(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	reader := blobstore.NewReader(c, bKey)
+	brd := blobstore.NewReader(c, bKey)
 
 	cnt := 0
-	var tasks = make([]*taskqueue.Task, 0, 30)
-	scanner := bufio.NewScanner(reader)
-	for scanner.Scan() {
-		//log.Infof(c, "%s", scanner.Text())
-		cnt++
-		tasks = append(tasks, &taskqueue.Task{
-			Path:    "/queue/data",
-			Payload: []byte(scanner.Text()),
-			Method:  "POST",
-		})
-		if cnt%30 == 0 {
-			log.Infof(c, "read count = %d", cnt)
-			_, err = taskqueue.AddMulti(c, tasks, "dataloader")
-			if err != nil {
-				log.Errorf(c, "add taskqueue : %v", err)
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			tasks = make([]*taskqueue.Task, 0, 30)
+	//var tasks = make([]*taskqueue.Task, 0, 30)
+	reader := bufio.NewReaderSize(brd, 1024*1024*10)
+	for {
+		line, isPrefix, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Errorf(c, "read line : %v, isPrefix = %v", err, isPrefix)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
 		}
+		if isPrefix {
+			log.Errorf(c, "read line isPrefix = true")
+			http.Error(w, "read line isPrefix = true", http.StatusBadRequest)
+			return
+		}
+		log.Infof(c, "start : %s", line[0:100])
+		log.Infof(c, "end : %s", line[len(line)-101:len(line)-1])
+
+		cnt++
+		t := &taskqueue.Task{
+			Header: http.Header{
+				"X-App-Version": {appengine.VersionID(c)},
+			},
+			Path:    "/queue/data",
+			Payload: line,
+			Method:  "POST",
+		}
+		_, err = taskqueue.Add(c, t, "dataloader")
+		if err != nil {
+			log.Errorf(c, "add taskqueue : %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		//		if cnt%30 == 0 {
+		//			log.Infof(c, "read count = %d", cnt)
+		//			_, err = taskqueue.AddMulti(c, tasks, "dataloader")
+		//			if err != nil {
+		//				log.Errorf(c, "add taskqueue : %v", err)
+		//				http.Error(w, err.Error(), http.StatusInternalServerError)
+		//				return
+		//			}
+		//			tasks = make([]*taskqueue.Task, 0, 30)
+		//		}
 	}
 	log.Infof(c, "read count = %d", cnt)
-	_, err = taskqueue.AddMulti(c, tasks, "dataloader")
-	if err != nil {
-		log.Errorf(c, "add taskqueue : %v", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	if err := scanner.Err(); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	//	_, err = taskqueue.AddMulti(c, tasks, "dataloader")
+	//	if err != nil {
+	//		log.Errorf(c, "add taskqueue : %v", err)
+	//		http.Error(w, err.Error(), http.StatusInternalServerError)
+	//		return
+	//	}
 
 	w.Write([]byte("done."))
 }
 
 func handlerDataPut(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
+
+	for k, v := range r.Header {
+		log.Infof(c, "%s:%s", k, v)
+	}
+	v := r.Header.Get("X-Appengine-Taskretrycount")
+	log.Infof(c, "value = %v", v)
+	if v != "0" {
+		log.Infof(c, "stop retry!")
+		w.WriteHeader(http.StatusOK)
+		return
+	}
 
 	if r.Method != "POST" {
 		http.Error(w, "POST only", http.StatusMethodNotAllowed)
@@ -100,16 +133,32 @@ func handlerDataPut(w http.ResponseWriter, r *http.Request) {
 
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Errorf(c, "body read error : %v", err)
+		log.Errorf(c, "read request body : %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	body := string(b)
 	log.Infof(c, "request body = %s", body)
-	arr := strings.Split(body, ",")
-	l := len(arr)
+
+	reader := csv.NewReader(strings.NewReader(body))
+	record, err := reader.Read()
+	if err != nil {
+		log.Errorf(c, "encode csv : %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	_, err = reader.Read()
+	if err != io.EOF {
+		log.Errorf(c, "multi line csv : %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	l := len(record)
 	log.Infof(c, "requry body array len = %d", l)
 
-	key := datastore.NewKey(nc, Kind, arr[0], 0, nil)
+	key := datastore.NewKey(nc, Kind, record[0], 0, nil)
 	q := datastore.NewQuery(Kind).Filter("__key__ =", key)
 	q = q.KeysOnly()
 	var samples []Sample
@@ -125,11 +174,11 @@ func handlerDataPut(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	values := make([]float64, len(arr)-1, len(arr)-1)
-	for i := 1; i < len(arr); i++ {
-		values[i-1], err = strconv.ParseFloat(arr[i], 64)
+	values := make([]float64, len(record)-1, len(record)-1)
+	for i := 1; i < len(record); i++ {
+		values[i-1], err = strconv.ParseFloat(record[i], 64)
 		if err != nil {
-			log.Errorf(c, "parse float: %v, value = %s", err, arr[i])
+			log.Errorf(c, "parse float: %v, value = %s", err, record[i])
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
